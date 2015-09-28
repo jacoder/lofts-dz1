9b1e0e137f89a4f7809b900818b04eeab3388bf5
Oleg Leontiev <oleg@leontiev.biz>
2015-09-28 19:46:27 +0400

    working gulp

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # lofts-dz1
-1. git clone github.com/jacoder/lofts-dz1.git
-2. cd
+1. git clone github.com/jacoder/lofts-dz1.git site
+2. cd site
+3. npm  i
+4. bower init
+5. gulp
--- gulpfile.js
+++ gulpfile.js
@@ -1,56 +1,63 @@
-var gulp=require('gulp');
-var concatCSS= require('gulp-concat-css');
-var rename= require('gulp-rename');
-var notify= require('gulp-notify');
-var connect= require('gulp-connect');
-var minifyCSS= require('gulp-minify-css');
-var liveReload= require('gulp-livereload');
-var browserSync=reuire('browser-sync');
+var gulp = require('gulp');
+var concatCSS = require('gulp-concat-css');
+var rename = require('gulp-rename');
+var notify = require('gulp-notify');
+var connect = require('gulp-connect');
+var minifyCSS = require('gulp-minify-css');
+var liveReload = require('gulp-livereload');
+var browserSync = require('browser-sync');
 
-gulp.task('connect',function(){
+gulp.task('connect', function() {
   connect.server({
     // root:'app',
-    root:'.',
-    livereload:true
-  } )
+    root: 'app',
+    livereload: true
+  })
 })
 
-gulp.task('server',function(){
+gulp.task('server', function() {
   browserSync({
-    port:9000,
-    server:{
-      baseDir:'app'
+    port: 9000,
+    server: {
+      baseDir: 'app'
     }
   });
 });
 
-gulp.task('css',function(){
-  gulp.src('css/*.css')
-  .pipe(concatCSS("bundle.css"))
-  // .pipe(minifyCSS("bundle.css"))
-  .pipe(minifyCSS(''))
-  .pipe(rename("bundle.min.css"))
-  .pipe(gulp.dest("app/css"))
-  .pipe(connect.reload())
+gulp.task('css', function() {
+  gulp.src('app/css/*.css')
+    .pipe(concatCSS("bundle.css"))
+    // .pipe(minifyCSS("bundle.css"))
+    .pipe(minifyCSS(''))
+    .pipe(rename("bundle.min.css"))
+    .pipe(gulp.dest("app/css"))
+    .pipe(connect.reload())
 
   // .pipe(livereload())
   // .pipe(notify('Done!'))
   ;
-}
-)
+})
 
-gulp.task('html',function(){
+gulp.task('html', function() {
     gulp.src('index.html')
-    .pipe(connect.reload())
-}
+      .pipe(connect.reload())
+  }
 
 )
 
 
-gulp.task('watch',function(){
-  gulp.watch('css/*.css',['css']);
-  gulp.watch('index.html',['html']);
+gulp.task('watch', function() {
+  gulp.watch([
+      'app/*.html',
+      'app/js/**/*.js',
+      'app/сss/**/*.css'
+    ])
+    .on('change', browserSync.reload);
+
+  // gulp.watch('css/*.css',['css']);
+  // gulp.watch('index.html',['html']);
 
 })
 
-gulp.task('default',['connect','html','css','watch'])
+// gulp.task('default', ['connect', 'html', 'css', 'watch'])
+gulp.task('default', ['server', 'watch']);
