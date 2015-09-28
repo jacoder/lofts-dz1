10c022bf2ba39150c40749ca5f1ae175360a03d5
Oleg Leontiev <oleg@leontiev.biz>
2015-09-28 16:26:21 +0400

    gulp reload set up

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,48 @@ var gulp=require('gulp');
 var concatCSS= require('gulp-concat-css');
 var rename= require('gulp-rename');
 var notify= require('gulp-notify');
+var connect= require('gulp-connect');
 var minifyCSS= require('gulp-minify-css');
+var liveReload= require('gulp-livereload');
 
-gulp.task('default',function(){
+
+
+gulp.task('connect',function(){
+  connect.server({
+    // root:'app',
+    root:'.',
+    livereload:true
+  } )
+})
+
+
+gulp.task('css',function(){
   gulp.src('css/*.css')
   .pipe(concatCSS("bundle.css"))
   // .pipe(minifyCSS("bundle.css"))
-  .pipe(minifyCSS())
+  .pipe(minifyCSS(''))
   .pipe(rename("bundle.min.css"))
-  .pipe(gulp.dest("out/"));
+  .pipe(gulp.dest("app/css"))
+  .pipe(connect.reload())
 
+  // .pipe(livereload())
+  // .pipe(notify('Done!'))
+  ;
 }
 )
+
+gulp.task('html',function(){
+    gulp.src('index.html')
+    .pipe(connect.reload())
+}
+
+)
+
 
 gulp.task('watch',function(){
-  gulp.watch('css/*.css',['default']);
-}
+  gulp.watch('css/*.css',['css']);
+  gulp.watch('index.html',['html']);
 
-)
+})
+
+gulp.task('default',['connect','html','css','watch'])
